9838bb90dc8c772b2ad0fdbbacfc84c1d9d25c75
Rishabharora2773 <rishabharora2773@gmail.com>
2018-01-18 23:46:41 +0530

    Project Submission-201501070

---
+++ 201501070_Rishab_Arora/DSL_Queries.py
@@ -0,0 +1,17 @@
+1. List all books:
+	res = es.search(index = INDEX_NAME, size=2, body={"query": {"match_all": {}}})
+
+2. List all the books with title containing word "Harry Potter":
+	res=es.search(index= INDEX_NAME,body={"query":{"match":{"title":"Harry Potter"}}})
+
+3. List all the books with title containing phrase "The:Millennium Trilogy":
+	res=es.search(index=INDEX_NAME,body={"query":{"match_phrase":{"title":"The:Millennium Trilogy"}}})
+
+4. List all the books with price greater than 500:
+	res=es.search(index=INDEX_NAME,body={"query":{"range":{"asp":{"gt":500}}}})
+
+5. List all the books whose author is "J.K.Rowling" and price greater than 500:
+	res=es.search(index=INDEX_NAME,body={"query":{"bool":{"must":{"author":"Rowling, J  K"},"must":{"range":{"asp":{"gt":500}}}}}})
+
+6. List all the books with class "Children fiction":
+	res=es.search(index=INDEX_NAME,body={"query":{"bool":{"must":{"match_all":{}},"must":{"match":{"product class":"Children's Fiction"}}}}})
