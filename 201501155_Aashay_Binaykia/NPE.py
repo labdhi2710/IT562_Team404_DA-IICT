9558da5cb02ef672b3dc52fa667732a9fb480c2e
Prakruti Joshi <prakrutijoshi10@gmail.com>
2018-02-24 19:22:49 +0530

    Add files via upload

---
+++ 201501155_Aashay_Binaykia/NPE.py
@@ -0,0 +1,34 @@
+import spacy
+import sys
+import json
+from spacy.tokens import Token
+
+'''
+This is a python code for Noun Phrase Extraction using spacy.
+We have implemented Noun Phrase Extraction on the long description of the Book from the dataset of books. 
+We had to first extract the text file from the json object.
+'''
+
+
+nlp=spacy.load('en_core_web_sm')
+
+#doc = nlp(u'Apple is looking at buying U.K. startup for $1 billion')
+
+#doc = nlp(u"I love data science on analytics vidhya")
+
+#
+
+for i in open("final.txt","rb"):
+    d=i.decode('utf8')
+    doc=nlp(d)
+    for word in doc:
+            print(word.text, word.lemma_)
+
+# Noun-Chunks
+
+for i in open("final.txt","rb"):
+    d=i.decode('utf8')
+    doc=nlp(d)
+    for word in doc.noun_chunks:
+        print(word.text)
+
